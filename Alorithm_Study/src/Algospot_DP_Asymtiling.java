a0425314fca4beae4da06fc25efbe86a40129b64
chshin33 <chshin33@gmail.com>
2017-01-06 23:36:56 +0900

    ..

--- Alorithm_Study/src/Algospot_DP_Asymtiling.java
+++ Alorithm_Study/src/Algospot_DP_Asymtiling.java
@@ -1,5 +1,4 @@
 /* 비대칭 타일링(문제ID : Asymtling)
- 
  사각형의 너비 n이 주어졌을때(2*n 크기), 비대칭 타일링 방법의 수를 
  1,000,000,007로 나눈 나머지 출력
 
@@ -9,48 +8,37 @@ Asymmetric(n) = 2*n 크기의 사각형을 채우는 비대칭 방법의 수
                                            - ( tiling(n) - tiling(n/2 - 1) + MOD ) % MOD
  
 Tiling(n) = 2*n 크기의 사각형을 타일로 덮는 방법
-
 Tiling(n) = Tiling(n-1) + Tiling(n-2)
-
- 
  */
 import java.io.FileInputStream;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Algospot_DP_Asymtiling {
-	
 	static int N;
 	static int Answer;
 	static final int MOD = 1000000007;
 	static int[] cache = new int[101];
-		
-	
+
 	public static void main(String[] args) throws Exception{
 		System.setIn(new FileInputStream("Algospot_DP_Asymtiling.txt"));
 		Scanner sc = new Scanner(System.in);
-		
 		int T = sc.nextInt();
-		
+
 		for(int test_case=1; test_case <=T; test_case++){
 			N = sc.nextInt();
-			
 			Arrays.fill(cache, -1);
 			Answer = asymmetric(N);
 			System.out.println("#" + test_case + " " + Answer);
 		}
-		
 		sc.close();
 	}
 	
 	static int asymmetric(int width){
-		
 		if(width % 2 ==1){
 //			int val = Math.abs(tiling(width) - tiling(width/2));
 			int val = tiling(width) - tiling(width/2) + MOD;
 			return val % MOD;
-			
-			
 		}
 		else{
 			int val =  tiling(width);

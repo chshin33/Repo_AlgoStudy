77e2fceb14f2e5d518cc1df3155124606c9ee559
chshin33 <chshin33@gmail.com>
2017-01-22 20:56:19 +0900

    ...

--- JavaExample/src/Math_ex.java
+++ JavaExample/src/Math_ex.java
@@ -3,21 +3,24 @@ public class Math_ex {
 
 	public static void main(String[] args) {
 
-		double n = 2.34456565;
+		double n = 2.34476565;
 		
 		System.out.println(n);
 		
-		//소수 4번째 자리에서 반올림하여 출력
-		n = Math.round(n*1000);
-		n = n/1000;
+		//소수 4번째 자리에서 반올림하여 3째자리까지 출력
+		double m = Math.round(n*1000);
+		m = m/1000;
 		
-		System.out.println(n);
+		System.out.println(m);
 		
-		/*double k;
-		k = n/1000;
-		//System.out.println(n);
-		//System.out.println(n/1000);
-		System.out.println(k);	
-	*/
+		//prinf 속성이용
+		System.out.printf("%.3f  \n", n);
+
+		
+		//long을 초기화 하기위해서는 숫자옆에 L을 붙여야 한다. 안붙이면, 컴파일러는 정수로 처리함
+		long num = 987654321987654321L;
+//		long num2 = 987654321987654321;
+		System.out.println(num);
+		
 	}
 }
